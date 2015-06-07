97cd8a0a9bd4c36e54318472ee86d67663fb1412
Sydney Weidman <sydney.weidman@gmail.com>
2015-06-07 11:13:06 -0500

    Initial commit

---
+++ djtut/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.conf import settings
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'djtut.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += (url(r'__debug__/', include(debug_toolbar.urls)),)
+
